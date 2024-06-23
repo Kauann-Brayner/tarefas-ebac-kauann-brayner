275f34a75650d278e8c4dece465716e60c834611
Brayner <155864168+Kauann-Brayner@users.noreply.github.com>
2024-06-23 01:11:17 -0300

    add src files

---
+++ src/scripts/index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector("#botao").addEventListener('click', function(){
+        document.querySelector("body").style.backgroundColor = "gray";
+    })
+})
